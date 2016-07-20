9ffb364d4dd79ebcfe1d03fdf6477cf66e3af399
DoubleDa <dayonxin@gmail.com>
2016-07-21 00:23:24 +0800

    Complate GifView Demo

--- GifView-Study-Analyse-Project/app/src/main/java/com/dyx/gsap/MainActivity.java
+++ GifView-Study-Analyse-Project/app/src/main/java/com/dyx/gsap/MainActivity.java
@@ -1,13 +1,39 @@
 package com.dyx.gsap;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.ant.liao.GifView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.gv_test)
+    GifView gvTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        initGifView();
+    }
+
+    private void initGifView() {
+        gvTest.setGifImage(R.mipmap.mygif1);
+        gvTest.setOnClickListener(this);
+        gvTest.setShowDimension(1400, 840);
+        gvTest.setGifImageType(GifView.GifImageType.COVER);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this, "点啥子嘛！", Toast.LENGTH_SHORT).show();
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GifView-Study-Analyse-Project
 GifView开源项目学习、分析原理项目
+
+[GifView:Android显示GIF动画](http://hao.jobbole.com/gifview/)
+
+
